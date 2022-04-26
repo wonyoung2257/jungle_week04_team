c7d2ab9387158bf6e9dcbc8eaf7f9bc74e156623
youngji <youngji.jang804@gmail.com>
2022-04-26 22:39:27 +0900

    재출

--- youngji/2655.py
+++ youngji/2655.py
@@ -1,8 +1,9 @@
 import sys
 
 n = int(sys.stdin.readline())
-a = [list(map(int,sys.stdin.readline().split())) for _ in range(n)] # 밑면넓이, 높이, 무게
+a2 = [list(map(int,sys.stdin.readline().split())) for _ in range(n)] # 밑면넓이, 높이, 무게
 
+a = sorted(a2,reverse=True)
 result = [0] * (n+1)
 hight = [0] * (n+1)
 for i in range(n) :
@@ -10,6 +11,9 @@ for i in range(n) :
     end_w,end_k = a[i][0],a[i][2]
     temp = [i+1]
     for j in range(n) :
+        # if a[j][0] < end_w and a[j][2] < end_k :
+        #     h += hight[j+1]
+        
         if a[j][0] < end_w and a[j][2] < end_k :
             h += a[j][1]
             end_w,end_k = a[j][0],a[j][2]
@@ -21,11 +25,11 @@ print(result)
 print(hight)
 
 maxi = hight.index(max(hight))
-print(maxi)
+# print(maxi)
 
 # 벽돌수, 벽돌 번호
-# print(len(result[maxi]))
-# for i in result[maxi][::-1] :
-#     print(i)
+print(len(result[maxi]))
+for i in result[maxi][::-1] :
+    print(i)
 
 
